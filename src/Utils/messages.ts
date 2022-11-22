5f7b76b344b63932cb93b13954f608210c8bec7e
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2022-11-22 01:26:31 -0800

    Update messages.ts

--- src/Utils/messages.ts
+++ src/Utils/messages.ts
@@ -373,8 +373,6 @@ export const generateWAMessageContent = async(
 				productImage: imageMessage,
 			}
 		})
-	} else if('listReply' in message) {
-		m.listResponseMessage = { ...message.listReply }
 	} else {
 		m = await prepareWAMessageMedia(
 			message,
@@ -449,15 +447,18 @@ export const generateWAMessageContent = async(
 	if('viewOnce' in message && !!message.viewOnce) {
 		m = { viewOnceMessage: { message: m } }
 	}
-	const [messageType] = Object.keys(m)
+
 	if('mentions' in message && message.mentions?.length) {
+		const [messageType] = Object.keys(m)
 		m[messageType].contextInfo = m[messageType] || { }
 		m[messageType].contextInfo.mentionedJid = message.mentions
 	}
-        if('contextInfo' in message && message) {
+
+	if('contextInfo' in message && message) {
+		const [messageType] = Object.keys(m)
 		m[messageType].contextInfo = message.contextInfo
 	}
-	
+
 	return WAProto.Message.fromObject(m)
 }
 
@@ -767,4 +768,37 @@ export const assertMediaContent = (content: proto.IMessage | null | undefined) =
 	}
 
 	return mediaContent
+}
+
+
+const generateContextInfo = () => {
+	const info: proto.IMessageContextInfo = {
+		deviceListMetadataVersion: 2,
+		deviceListMetadata: { }
+	}
+	return info
+}
+
+/**
+ * this is an experimental patch to make buttons work
+ * Don't know how it works, but it does for now
+ */
+export const patchMessageForMdIfRequired = (message: proto.IMessage) => {
+	const requiresPatch = !!(
+		message.buttonsMessage
+	        || message.templateMessage
+		|| message.listMessage
+	)
+	if(requiresPatch) {
+		message = {
+			viewOnceMessage: {
+				message: {
+					messageContextInfo: generateContextInfo(),
+					...message
+				}
+			}
+		}
+	}
+
+	return message
 }
